b0471147c28bfac59a27020bdd765071d7cadae1
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-03-26 13:46:58 +0100

    Exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,23 @@
 // you can write js here
 
 console.log('exo-5');
+
+var input = "Bonjour je fais des prouts";
+
+var vowels = ["a", "e", "y", "u", "i", "o"];
+
+var resultArray = [null];
+
+for (var i = 0; i < input.length; i++)
+{
+    vowels.forEach(function (vowel)
+    {
+        if(input.charAt(i) == vowel)
+        {
+            resultArray.push(input.charAt(i));
+        }
+    });
+}
+
+resultArray.shift();
+console.log(resultArray.join(" ").toUpperCase());
