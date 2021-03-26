dab98f77a72b90e262a188c510762089c424623a
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-03-26 11:59:22 +0100

    Exo 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,49 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+function deleteLastElement()
+{
+    secretMessage.pop();
+}
+
+function addToProgram()
+{
+    secretMessage.push("to");
+    secretMessage.push(("program"));
+}
+
+function replaceEasilyToRight()
+{
+    var pos = secretMessage.indexOf("easily");
+    secretMessage[pos] = "right";
+}
+
+function deleteFirstElement()
+{
+    secretMessage.shift();
+}
+
+function addProgramming()
+{
+secretMessage.unshift("Programming");
+}
+
+function replaceGetRightTheFirstTimeToKnow()
+{
+    var pos = secretMessage.indexOf("get");
+    var nbElement = 4;
+
+    secretMessage.splice(pos, nbElement);
+
+    pos = secretMessage.indexOf("time,");
+    secretMessage[pos] = "know";
+}
+
+deleteLastElement();
+addToProgram();
+replaceEasilyToRight();
+deleteFirstElement();
+addProgramming();
+replaceGetRightTheFirstTimeToKnow();
+console.log(secretMessage.join(' '));
